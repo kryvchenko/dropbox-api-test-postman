a985f9fe9f9ffee13f637b9716027b6cf91d1d51
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-10-04 12:05:18 +0200

    readme updated

--- assets/data.js
+++ assets/data.js
@@ -1,3 +1,3 @@
 
-// eLtsUaY8RPIAAAAAAAAAAaDBNcGbIOuXYKGP9Hhn6HE7GCESe9SKFWs8NI_ToCum
-// qwn2tzzgruh1amt:aghea2e8ir6dtpt
+//refresh => eLtsUaY8RPIAAAAAAAAAAaDBNcGbIOuXYKGP9Hhn6HE7GCESe9SKFWs8NI_ToCum
+// base64 => qwn2tzzgruh1amt:aghea2e8ir6dtpt
